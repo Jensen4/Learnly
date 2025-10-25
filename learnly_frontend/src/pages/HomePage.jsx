b845f2c05e82d9e47f18110f1b6615e768fdd98a
caleb-jung123 <caleb10131013@gmail.com>
2025-10-25 16:21:57 -0400

    done

---
+++ learnly_frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from '../components/Home'
+import NavBar from '../components/NavBar'
+
+export default function HomePage() {
+    return (
+        <div>
+            <NavBar />
+            <Home />
+        </div>
+    )
+}
