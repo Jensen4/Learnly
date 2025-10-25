90204e6f4860fe2e229859174be079a2c222e903
caleb-jung123 <caleb10131013@gmail.com>
2025-10-25 15:03:29 -0400

    changing routing system

--- learnly_frontend/src/App.jsx
+++ learnly_frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
