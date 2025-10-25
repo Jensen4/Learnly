3902db3bb2174d4a856f119102934e5f94fb3816
Jensen4 <jensensung12@gmail.com>
2025-10-25 15:56:00 -0400

    finished home page

---
+++ learnly_frontend/src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import { MdFileUpload } from "react-icons/md";
+
+export default function Home() {
+    const [fileType, setFileType] = useState("");
+
+    function handleUpload() {
+        console.log("uploaded");
+    }
+
+    function handleSubmit() {
+        console.log("submitted");
+    }
+
+    return (
+        <div className='min-h-screen flex items-center justify-center bg-zinc-100'>
+            <div className='flex flex-col space-x-10 h-[80vh] w-[80vw]'>
+                {/* <button onClick={handleUpload} className='w-50 h-50 flex flex-col items-center justify-center space-y-2 p-4 cursor-pointer text-4xl font-bold border-3 border-zinc-400 rounded-xl'>
+                    <FaRegStickyNote />
+                    <p className="text-[30px] font-light">Upload Notes</p>
+                </button> */}
+                <label htmlFor="fileUpload" className="cursor-pointer h-full w-full bg-zinc-300 rounded-xl flex flex-col items-center justify-center space-y-10">
+                    <MdFileUpload  className="scale-500" />
+                    <p className="text-4xl">Add a file to upload</p>
+                </label>
+                <input id="fileUpload" type="file" onChange={handleUpload} className='hidden' />
+                <span className="flex flex-row justify-between py-5 text-2xl">
+                    <select className="cursor-pointer" onChange={(e) => {setFileType(e.option.value)}}>
+                        <option value="notes">Notes</option>
+                        <option value="lecture">Lecture</option>
+                        <option value="readings">Readings</option>
+                    </select>
+                    <button onClick={handleSubmit} className="cursor-pointer bg-green-400 rounded-2xl p-4">
+                        <p className="">Submit File</p>
+                    </button>
+                </span>
+            </div> 
+        </div>
+    )
+}
